38a220e38e52aafea70d2076d2699eca549e4565
anaconda-pkg-build <66446536+anaconda-pkg-build@users.noreply.github.com>
2021-11-17 16:11:53 +0100

    jupyter_client 7.0.6 (#9)
    * DOR update to 7.0.6
    
    * Fix python in host and add in test/rrequires, add missing packages, update pytest command
    
    * Update test/requires
    
    * Rebuild with updated dependencies
    
    * Increase ulimit on osx64
    
    * Increase ulimit
    
    * Increase ulimit
    
    * Increase ulimit
    
    * Change ulimit
    
    * Test ulimit for macOS (#10)
    
    * Split tests in order to raise ulimit on osx
    
    * remove ulimit in meta.yaml
    
    * Move pip check to run_test.*
    
    * Small change
    
    * Fix python in host and add in test/rrequires, add missing packages, update pytest command
    
    * parent e97b9e5a9950c02002f897b5d83566f7cb1d8d56
    author Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com> 1636711406 +0200
    committer Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com> 1637160416 +0200
    
    Fix python in host and add in test/rrequires, add missing packages, update pytest command
    
    Update test/requires
    
    Rebuild with updated dependencies
    
    Test ulimit for macOS (#10)
    
    * Split tests in order to raise ulimit on osx
    
    * remove ulimit in meta.yaml
    
    * Move pip check to run_test.*
    
    Small change
    
    * Update the recipe
    
    Co-authored-by: Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com>
    Co-authored-by: Anthony DiPietro <51377278+adipietro18@users.noreply.github.com>

---
+++ recipe/run_test.bat
@@ -0,0 +1,13 @@
+@echo on
+
+python -m pip check
+if errorlevel 1 exit 1
+
+jupyter kernelspec list
+if errorlevel 1 exit 1
+
+jupyter run -h
+if errorlevel 1 exit 1
+
+pytest --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered
+if errorlevel 1 exit 1
