cf9665a6ace9b41fa8fc3dfe3f31031cbefe87c7
Serhii Kupriienko <skupriienko@anaconda.com>
2022-04-21 08:44:16 +0000

    Skip more tests on s390x

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -14,16 +14,24 @@ elif [ "$(uname -m)" = "ppc64le" ]; then
     pytest --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered --no-cov-on-fail \
     -k "not (test_start_parallel_process_kernels[tcp] \
     or test_async_signal_kernel_subprocesses[signaltest-_install_kernel-_ShutdownStatus.ShutdownRequest] \
-    or test_start_sequence_kernels[tcp] or test_start_sequence_kernels[ipc] \
+    or test_start_sequence_kernels[tcp] \ 
+    or test_start_sequence_kernels[ipc] \
     or test_start_parallel_thread_kernels[tcp] \
-    or test_start_sequence_process_kernels[tcp] or test_signal_kernel_subprocesses \
+    or test_start_sequence_process_kernels[tcp] \ 
+    or test_signal_kernel_subprocesses \
     or test_start_new_async_kernel)"
 # Timeout issues or RuntimeError: Kernel died before replying to kernel_info (on s390x)
 elif [ "$(uname -m)" = "s390x" ]; then
-    pytest --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered --no-cov-on-fail -k "not (test_start_parallel_process_kernels[tcp] \
-    or test_signal_kernel_subprocesses[signaltest-_install_kernel-_ShutdownStatus.ShutdownRequest] \
-    or test_start_sequence_process_kernels[tcp] \
-    or test_start_sequence_kernels[tcp])" 
+    pytest --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered --no-cov-on-fail \
+    -k "not (test_start_parallel_process_kernels[tcp] \
+    or test_async_signal_kernel_subprocesses[signaltest-_install_kernel-_ShutdownStatus.ShutdownRequest] \
+    or test_start_sequence_kernels[tcp] \ 
+    or test_start_sequence_kernels[ipc] \
+    or test_start_parallel_thread_kernels[tcp] \
+    or test_start_sequence_process_kernels[tcp] \ 
+    or test_signal_kernel_subprocesses \
+    or test_start_new_async_kernel
+    or test_shutdown)"
 else
     pytest --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered --no-cov-on-fail 
 fi
