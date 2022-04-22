ba7f5cc532d15a5815783bd95d9ce2bc156063bf
Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com>
2022-04-22 13:09:28 +0300

    jupyter_client 7.2.2 (#15)
    * Remove noarch, update dependencies, add test/commands
    
    * Update tests
    
    * Update test/requires and skip some tests on ppc64le
    
    * Skip tests on s390x
    
    * Add build number

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -9,5 +9,5 @@ if errorlevel 1 exit 1
 jupyter run -h
 if errorlevel 1 exit 1
 
-pytest --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered
+pytest -vv --pyargs jupyter_client
 if errorlevel 1 exit 1
