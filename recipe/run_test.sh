d465b9b831f9e021b76ea9b7f515fdf3f14b2cab
ViridianMelody <mlove@anaconda.com>
2023-02-02 15:31:08 -0700

    Skip async tests causing timeouts

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -11,17 +11,8 @@ jupyter run -h
 if [ "$(uname)" == "Darwin" ]; then
     ulimit -n 4096
     pytest --asyncio-mode=auto --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered --no-cov-on-fail -k "not (test_install_kernel_spec_prefix)" 
-# Timeout issues with python 3.10 on ppc64le
-elif [ "$(uname -m)" = "ppc64le" ]; then
-    pytest --asyncio-mode=auto --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered --no-cov-on-fail \
-    -k "not (test_start_parallel_process_kernels[tcp] \
-    or test_async_signal_kernel_subprocesses[signaltest-_install_kernel-_ShutdownStatus.ShutdownRequest] \
-    or test_start_sequence_kernels[tcp] or test_start_sequence_kernels[ipc] \
-    or test_start_parallel_thread_kernels[tcp] \
-    or test_start_sequence_process_kernels[tcp] or test_signal_kernel_subprocesses \
-    or test_start_new_async_kernel)"
-# Timeout issues or RuntimeError: Kernel died before replying to kernel_info (on s390x)
-elif [ "$(uname -m)" = "s390x" ]; then
+# Timeout issues on most platforms
+else
     pytest --asyncio-mode=auto --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered --no-cov-on-fail \
     -k "not (test_start_parallel_process_kernels[tcp] \
     or test_async_signal_kernel_subprocesses[signaltest-_install_kernel-_ShutdownStatus.ShutdownRequest] \
@@ -29,6 +20,4 @@ elif [ "$(uname -m)" = "s390x" ]; then
     or test_start_parallel_thread_kernels[tcp] \
     or test_start_sequence_process_kernels[tcp] or test_signal_kernel_subprocesses \
     or test_start_new_async_kernel or test_shutdown or test_restart_check[tcp])"
-else
-    pytest --asyncio-mode=auto --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered --no-cov-on-fail 
 fi
