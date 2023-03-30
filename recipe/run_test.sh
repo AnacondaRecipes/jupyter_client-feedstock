9df9cab496b706c4c6323aa4f2225f780b5ee3f6
Anton Kabanovs <123631355+akabanovs@users.noreply.github.com>
2023-03-30 11:23:30 +0100

    Update to v 8.1.0: (#24)
    * Update to v 8.1.0:
     - version bumped up and sha256 updated
     - script command modified to include --no-build-isolation
     - dependencies revised and updated
     - tests and run_tests.sh/bat scripts modified

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -10,14 +10,14 @@ jupyter run -h
 
 if [ "$(uname)" == "Darwin" ]; then
     ulimit -n 4096
-    pytest --asyncio-mode=auto --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered --no-cov-on-fail -k "not (test_install_kernel_spec_prefix)" 
+    pytest tests --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered --no-cov-on-fail -k "not test_tcp_cinfo"
 # Timeout issues on most platforms
 else
-    pytest --asyncio-mode=auto --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered --no-cov-on-fail -vv \
-    -k "not (test_start_parallel_process_kernels[tcp] \
+    pytest tests --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered --no-cov-on-fail -vv \
+    -k "not (test_start_parallel_process_kernels \
     or test_async_signal_kernel_subprocesses[signaltest-_install_kernel-_ShutdownStatus.ShutdownRequest] \
-    or test_start_sequence_kernels[tcp] or test_start_sequence_kernels[ipc] \
-    or test_start_parallel_thread_kernels[tcp] \
-    or test_start_sequence_process_kernels[tcp] or test_signal_kernel_subprocesses \
-    or test_start_new_async_kernel or test_shutdown or test_restart_check[tcp])"
+    or test_start_parallel_thread_kernels \
+    or test_input_request \
+    or test_load_ips \
+    or test_tcp_cinfo)"
 fi
