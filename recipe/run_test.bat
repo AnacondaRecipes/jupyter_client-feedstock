f32d7fce8405be988109004385fb108cad38f8ef
Serhii Kupriienko <skupriienko@anaconda.com>
2025-11-11 13:46:02 +0200

    Skip tests for py314 on win

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -9,5 +9,11 @@ if errorlevel 1 exit 1
 jupyter run -h
 if errorlevel 1 exit 1
 
-pytest tests -vv --pyargs jupyter_client
+IF "%PY_VER%"=="3.14" (
+	    echo "WARNING: Skipping tests for Python 3.14. Remove it when ipykernel & pytest-jupyter-client are available for Python 3.14."
+) ELSE (
+    pytest tests -vv --pyargs jupyter_client
+)
+
+
 if errorlevel 1 exit 1
