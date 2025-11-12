8c65abe335283480d75af4b323a500738e90195c
Serhii Kupriienko <skupriienko@anaconda.com>
2025-11-12 12:10:04 +0200

    Skip the same tests on Windows too

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -12,7 +12,7 @@ if errorlevel 1 exit 1
 IF "%PY_VER%"=="3.14" (
 	    echo "WARNING: Skipping tests for Python 3.14. Remove it when ipykernel & pytest-jupyter-client are available for Python 3.14."
 ) ELSE (
-    pytest tests -vv --pyargs jupyter_client
+    pytest tests -vv --pyargs jupyter_client -k "not (test_start_parallel_process_kernels or test_async_signal_kernel_subprocesses[signaltest-_install_kernel-_ShutdownStatus.ShutdownRequest] or test_start_parallel_thread_kernels or test_input_request or test_load_ips or test_tcp_cinfo)"
 )
 
 
