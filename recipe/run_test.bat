70cac8638dda48af220f11b4c98f2f45c270a58d
skupr-anaconda <79282962+skupr-anaconda@users.noreply.github.com>
2025-11-12 12:22:24 +0200

    jupyter_client 8.6.3: Rebuild for py314 (#30)

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -9,5 +9,11 @@ if errorlevel 1 exit 1
 jupyter run -h
 if errorlevel 1 exit 1
 
-pytest tests -vv --pyargs jupyter_client
+IF "%PY_VER%"=="3.14" (
+	    echo "WARNING: Skipping tests for Python 3.14. Remove it when ipykernel & pytest-jupyter-client are available for Python 3.14."
+) ELSE (
+    pytest tests -vv --pyargs jupyter_client -k "not (test_start_parallel_process_kernels or test_async_signal_kernel_subprocesses[signaltest-_install_kernel-_ShutdownStatus.ShutdownRequest] or test_start_parallel_thread_kernels or test_input_request or test_load_ips or test_tcp_cinfo)"
+)
+
+
 if errorlevel 1 exit 1
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -11,11 +11,20 @@ jupyter run -h
 if [ "$(uname)" == "Darwin" ]; then
     ulimit -n 4096
 fi
-# Timeout issues on most platforms
-pytest tests --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered --no-cov-on-fail -vv \
--k "not (test_start_parallel_process_kernels \
-or test_async_signal_kernel_subprocesses[signaltest-_install_kernel-_ShutdownStatus.ShutdownRequest] \
-or test_start_parallel_thread_kernels \
-or test_input_request \
-or test_load_ips \
-or test_tcp_cinfo)"
+
+PYTHON_MINOR_VERSION=$(python -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
+echo "PYTHON_MINOR_VERSION: $PYTHON_MINOR_VERSION"
+
+# Workaround for the python 3.14 build-out because ipykernel & pytest-jupyter-client are circular dependencies
+if [ "$PYTHON_MINOR_VERSION" == "3.14" ]; then
+    echo "WARNING: Skipping tests for Python 3.14. Remove it when ipykernel & pytest-jupyter-client is available for Python 3.14."
+else
+    # Timeout issues on most platforms
+    pytest tests --pyargs jupyter_client --cov jupyter_client --cov-report term-missing:skip-covered --no-cov-on-fail -vv \
+    -k "not (test_start_parallel_process_kernels \
+    or test_async_signal_kernel_subprocesses[signaltest-_install_kernel-_ShutdownStatus.ShutdownRequest] \
+    or test_start_parallel_thread_kernels \
+    or test_input_request \
+    or test_load_ips \
+    or test_tcp_cinfo)"
+fi
